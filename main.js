05e0d3ed4c1e04a91b8c27dccbf69e72991afa63
Robert L. Read <read.robert@gmail.com>
2015-09-04 11:47:11 -0500

    Updating license

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+/*
+
+    CoilChoice: A tool for understanding and desiging electromagnetic coils for MMF
+
+    Copyright (C) 2015  Robert L. Read
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
 $(document).ready(function(){
   
   var margin = {top: 10, right: 10, bottom: 100, left: 40},
